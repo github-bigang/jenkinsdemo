49eeafce3ca84c8b515eae49ef624ca55c513dc0
bigang2021 <1174601212@qq.com>
2021-02-09 10:41:34 +0800

    dev

--- src/main/java/com/ego/jenkinsdemo/Test02.java
+++ src/main/java/com/ego/jenkinsdemo/Test02.java
@@ -3,5 +3,6 @@ package com.ego.jenkinsdemo;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("Test.......");
+        System.out.println("dev.......");
     }
 }
