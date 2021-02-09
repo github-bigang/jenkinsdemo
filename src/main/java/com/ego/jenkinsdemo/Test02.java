e14cff397e06869a7e606a4fae4a9d83489177fe
bigang2021 <1174601212@qq.com>
2021-02-09 09:13:40 +0800

    第一次 提交 test02 到 dev02分支

--- src/main/java/com/ego/jenkinsdemo/Test02.java
+++ src/main/java/com/ego/jenkinsdemo/Test02.java
@@ -2,6 +2,6 @@ package com.ego.jenkinsdemo;
 
 public class Test02 {
     public static void main(String[] args) {
-        System.out.println("Test.......");
+        System.out.println("Test02.......");
     }
 }
